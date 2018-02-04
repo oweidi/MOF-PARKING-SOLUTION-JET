3f0e064ed619b2080c68ae73e87f4a181b2cdab6
Oweidi <abdallah.oweidi@gmail.com>
2018-02-04 11:34:28 +0300

    Add genereate war file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,21 @@
-/**
-  Copyright (c) 2015, 2017, Oracle and/or its affiliates.
-  The Universal Permissive License (UPL), Version 1.0
-*/
 'use strict';
-
 var path = require('path');
-
 module.exports = function(grunt) {
 
   require('load-grunt-config')(grunt, {
-  	configPath: path.join(process.cwd(), 'scripts/grunt/config')
+    configPath: path.join(process.cwd(), 'scripts/grunt/config'),
+    data: {
+      appname: path.basename(process.cwd()),  // same as project directory name, accessible with '<%= appname %>'
+      appdir: 'web',  // accessible with '<%= appdir %>'
+      distdir: 'dist'  // accessible with '<%= distdir %>'
+    }
   });
-
-  grunt.loadNpmTasks("@oracle/grunt-oraclejet");
-
+  grunt.loadNpmTasks("grunt-oraclejet");
+  //grunt.loadNpmTasks("grunt-war");
   grunt.registerTask("build", "Public task. Calls oraclejet-build to build the oraclejet application. Can be customized with additional build tasks.", function (buildType) {
-    grunt.task.run([`oraclejet-build:${buildType}`]);
+    grunt.task.run([`oraclejet-build:${buildType}`, 'war']);
   });
-
   grunt.registerTask("serve", "Public task. Calls oraclejet-serve to serve the oraclejet application. Can be customized with additional serve tasks.", function (buildType) {
     grunt.task.run([`oraclejet-serve:${buildType}`]);
-  }); 
+  });
 };
-
